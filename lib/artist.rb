8720971e8ae58e72140fd124aabb6e426004de9d
TJZii <tjhaweszii@gmail.com>
2020-12-06 17:44:27 -0800

    commit tree 9643d701382fdd449aff7412a8d1a744ca18f9d0

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+
+    attr_accessor :name, :songs
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        @songs << self
+    end
+
+    def songs(artist)
+
+    end
+
+end
